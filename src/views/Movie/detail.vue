3dd06d84f432bf57265670cd81ee162df09f579b
xiayw1996 <xiayangwei1996@qq.com>
2020-01-15 10:14:35 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -37,7 +37,7 @@
         </ul>
       </div>-->
       <div class="detail_list">
-        <div class="detail_list_bg"></div>
+        <div class="detail_list_bg" :style="{'background-image': 'url('+ detailMovie.img.replace(/w\.h/,'148.208') +')'}"></div>
         <div class="detail_list_filter"></div>
         <div class="detail_list_content">
           <div class="detail_list_img">
@@ -127,7 +127,7 @@ export default {
   background: white;
 }
 #detailConrainer.slide-enter-active {
-  animation: 3s slideMove;
+  animation: 0.5s slideMove;
 }
 @keyframes slideMove {
   0% {
@@ -150,7 +150,7 @@ export default {
 .detail_list .detail_list_bg {
   width: 100%;
   height: 100%;
-  background: url(/images/movie_1.jpg) 0 40%;
+  background: 0 40%;
   filter: blur(20px);
   background-size: cover;
   position: absolute;
